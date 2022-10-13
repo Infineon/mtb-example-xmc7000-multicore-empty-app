1379d420b81d850f21e9f3625018a18d8b37f7d7
gitlab-runner <do-not-reply@cypress.com>
2022-10-13 06:13:44 -0500

    Upload mtb-example-xmc7000-multicore-empty-app 1.0.0.374

--- proj_cm0p/main.c
+++ proj_cm0p/main.c
@@ -41,7 +41,6 @@
 *******************************************************************************/
 
 #include "cy_pdl.h"
-#include "cycfg.h"
 #include "cyhal.h"
 #include "cybsp.h"
 
@@ -50,19 +49,24 @@
 
 int main(void)
 {
+    cy_rslt_t result;
+
+    /* Initialize the device and board peripherals */
+    result = cybsp_init() ;
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
     /* enable interrupts */
     __enable_irq();
 
     /* Enable CM7_0/1. CY_CORTEX_M7_APPL_ADDR is calculated in linker script, check it in case of problems. */
     Cy_SysEnableCM7(CORE_CM7_0, CY_CORTEX_M7_0_APPL_ADDR);
-    Cy_SysLib_Delay(1000); /* To avoid race between CM7's while initializing the clocks */
 
 #if CM7_DUAL
     Cy_SysEnableCM7(CORE_CM7_1, CY_CORTEX_M7_1_APPL_ADDR);
 #endif /* CM7_DUAL */
-
-    SystemCoreClockUpdate();
-    Cy_SysPm_CpuSleepOnExit(true);
 
     for(;;)
     {
